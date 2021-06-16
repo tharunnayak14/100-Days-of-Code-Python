055ea9b271b4df5eebba92b1b48e4670ac65c42a
tharunnayak14 <tharunbadavath14@gmail.com>
2021-06-16 09:15:27 +0530

    Day-6

---
+++ Day-6/Functions.py
@@ -0,0 +1,6 @@
+# defining function
+def my_function():
+    print("Hello this is a function")
+    print("Bye")
+# calling function
+my_function()
---
+++ Day-6/while_loops.py
@@ -0,0 +1,22 @@
+# while something true
+#    repeat
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#
+# while at_goal() != True:
+#     move()
+#     jump()
+
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds/menus/reeborg_intro_en.json&name=Hurdle%202&url=worlds/tutorial_en/hurdle2.json
