2141c77c8915a0257e43e4fc585263e1ab82e032
tharunnayak14 <tharunbadavath14@gmail.com>
2021-06-20 19:34:40 +0530

    Day-10

---
+++ Day-10/functions_with_output.py
@@ -0,0 +1,7 @@
+def my_function():
+    result = 2 * 3
+    return result
+
+
+ans = my_function()
+print(ans)
---
+++ Day-10/multiple_return.py
@@ -0,0 +1,12 @@
+# Return as an early exit
+def format_name(f_name, l_name):
+    """Take a first and last name and format it
+    to return the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result: {formated_f_name} {formated_l_name}"
+
+name = format_name(input("Enter first name:\n"), input("Enter last name:\n"))
+print(name)
