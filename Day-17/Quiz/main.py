4594d216b5c7b748cd91219153ff92b76eddcbcb
tharunnayak14 <tharunbadavath14@gmail.com>
2021-06-28 10:32:49 +0530

    Day-16

---
+++ Day-17/Quiz/main.py
@@ -0,0 +1,31 @@
+from question_data import question_data
+from quiz_brain import QuizBrain
+
+questions = question_data
+
+
+class NewQuestion:
+    def __init__(self, text, answer):
+        # question = random.choice(questions)
+        self.text = text
+        self.answer = answer
+
+
+# que_1 = NewQuestion("question", "answer")
+# print(que_1.text, que_1.answer)
+
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    question_bank.append(NewQuestion(question_text, question_answer))
+
+# print(question_bank)
+Quiz = QuizBrain(question_bank)
+while Quiz.still_has_questions():
+    Quiz.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score is {Quiz.score}/{len(Quiz.question_list)}")
---
+++ Day-17/own_class_creation.py
@@ -0,0 +1,53 @@
+# class User:
+#     pass
+#
+#
+# # object
+# user_1 = User()
+#
+# print(user_1)
+#
+# user_1.id = 100
+# user_1.name = "Tharun"
+#
+# print(user_1.name)
+
+
+# we can use a constructor to initialise the attributes
+
+class User:
+    # self, user_id, username  are attributes
+    # init function is the constructor
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.name = username
+        self.followers = 0
+
+
+# object
+user_1 = User(420, "Tharun")
+
+# print(user_1)
+print(user_1.name)
+
+
+class Car:
+    def __init__(self, seats, name):
+        # runs everytime we initialise the object
+        self.name = name
+        self.seats = seats
+        print(f"New {self.name} car created")
+        
+    # methods
+    # method always has a self parameter
+    def enter_race_mode(self, name):
+        print(f"{name} Entered race mode")
+        self.seats = 2
+
+
+BMW = Car(5, "BMW")
+print(BMW.seats)
+BMW.enter_race_mode("BMW")
+print(BMW.seats)
+AUDI = Car(7, "AUDI")
+print(AUDI.seats)
